a21c76578aac1cf3bb8d5e661d1264f69f4adb82
rehemafaith <rehemafaith01@gmail.com>
2019-04-09 07:31:28 +0300

    create function added

--- cred.py
+++ cred.py
@@ -2,10 +2,35 @@ class Credential :
   '''
   Class that has the user's credentials
   '''
+  credential_list =[]
+
+  def __init__(self,username,number,password,confirmation_password):
+    
+    self.username =username
+    self.phone_number = number
+    self.password = password
+    self.confirmation_password =confirmation_password
+
+
+  def save_credential(self):
+      '''
+      save_credential method saves contact objects into credential_list
+      '''
+      Credential.credential_list.append(self)
+
+
+  def delete_credential(self):
+    '''
+    delete_credential method deletes a saved contact from the contact_list 
+    '''
+
+    Credential.credential_list.remove(self)
+
+  
   @classmethod
   def find_by_username(cls,username):
     '''
-    methos that takes in anumber and returns a contact 
+    method that takes in number and returns a contact 
     that matches that number 
 
     Args:
@@ -17,20 +42,9 @@ class Credential :
       if credential.username == username:
         return credential
 
-  credential_list =[] 
+   
 
-  def save_credential(self):
-      '''
-      save_credential method saves contact objects into credential_list
-      '''
-      Credential.credential_list.append(self)
-
-  def __init__(self,username,number,password,confirmation_password):
-    
-    self.username =username
-    self.phone_number = number
-    self.password = password
-    self.confirmation_password =confirmation_password
-
+  
+  
   
   
